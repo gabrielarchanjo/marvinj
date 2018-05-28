1013622340ddaf5f07bc442acc07d203d22ba6f2
gabrielarchanjo <leirbag.arc@gmail.com>
2018-05-28 18:10:27 -0300

    convert image to blob for uploading

--- marvinj/src/image/MarvinImage.js
+++ marvinj/src/image/MarvinImage.js
@@ -266,3 +266,28 @@ MarvinImage.prototype.draw = function(canvas, x, y, alphaCombination){
 		canvas.getContext("2d").drawImage(img, x, y);
 	}*/
 };
+
+MarvinImage.prototype.toBlob = function(){
+	this.update();
+	return MarvinImage.dataURItoBlob(this.canvas.toDataURL("image/png"));
+};
+
+MarvinImage.dataURItoBlob = function(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
